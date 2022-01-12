a052153602b2c0a19d535e3343eda9dc189d454d
Aman Singh <amankumarsingh.ae@gmail.com>
2022-01-12 08:18:40 +0530

    Added .gitignore

--- sorting.py
+++ sorting.py
@@ -1,15 +1,26 @@
-def insertion_sort(A):
-    """Assumes A is a list of integers
-       Returns the sorted A in ascending order.
+def insertion_sort(A, reverse = False):
+    """Assumes A is a list of integers, by default reverse = False
+       Returns the sorted A in non-decreasing order if reverse = False, 
+       and in non-increasing order if reverse = True.
        ****************************************
        The complexity of the algorithm is O(n^2)."""
-    for j in range(1, len(A)):
-        key = A[j]
-        i = j - 1
-        while i >= 0 and A[i] > key:
-            A[i+1] = A[i]
-            i = i-1
-        A[i+1] = key
-    return A
+    if reverse == False:
+        for j in range(1, len(A)):
+            key = A[j]
+            i = j - 1
+            while i >= 0 and A[i] > key:
+                A[i+1] = A[i]
+                i = i-1
+            A[i+1] = key
+        return A
+    else:
+        for j in range(1, len(A)):
+            key = A[j]
+            i = j - 1
+            while i >= 0 and A[i] < key:
+                A[i+1] = A[i]
+                i = i - 1
+            A[i+1] = key
+        return A
 
 # print(insertion_sort([5,1,5,2,5,1,4,5]))
