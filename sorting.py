0f45cacaa38d72e9e425ac9ddceda3980d32dba1
Aman Singh <amankumarsingh.ae@gmail.com>
2022-01-14 08:42:14 +0530

    Implemented Brute Force to find the index of the first occurance of a query within a list

--- sorting.py
+++ sorting.py
@@ -23,4 +23,31 @@ def insertion_sort(A, reverse = False):
             A[i+1] = key
         return A
 
-# print(insertion_sort([5,1,5,2,5,1,4,5]))
+# print(insertion_sort([5,1,5,2,5,1,4,5], True))
+
+# Merge-sort
+
+def merge(A, p, q, r):
+    """Assumes A to be an array of numbers, p, q, and r are indices of the array A such that that
+       p <= q < r. The process also assumes that the subarrays A[p, q] and A[q+1, r] are sorted."""
+    n1 = q-p+1
+    n2 = r-q
+    L, R = [], []
+    for i in range(n1):
+        L.append(A[p+i])
+    for j in range(n2):
+        R.append(A[q+j+1])
+    L.append(1000)
+    R.append(1000)
+    i = 0
+    j = 0
+    for k in range(p, r+1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+    return A
+
+print(merge([5,6,8,12,15,16,2,5,8,6,7], 0, 5, 10))
